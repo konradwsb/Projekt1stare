661fd77757fb9fdd745f742aa02d10330fd27cb3
konradwsb <161937100+konradwsb@users.noreply.github.com>
2024-06-01 11:07:55 +0200

    Add files via upload

---
+++ src/test.ts
@@ -0,0 +1,18 @@
+import 'zone.js/testing';
+import { getTestBed } from '@angular/core/testing';
+import {
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting
+} from '@angular/platform-browser-dynamic/testing';
+
+declare const require: any;
+
+// First, initialize the Angular testing environment.
+getTestBed().initTestEnvironment(
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting(),
+);
+
+// Then we find all the tests.
+const context = (require as any).context('./', true, /\.spec\.ts$/);
+context.keys().map(context);
